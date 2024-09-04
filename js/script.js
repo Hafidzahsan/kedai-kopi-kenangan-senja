24000057768cae49396f35c88bc35bbb52918566
Hafidz Aslami Ahsan <hafidzahsan20@gmail.com>
2024-09-04 13:52:13 +0700

    Update pemesanan

--- js/script.js
+++ js/script.js
@@ -14,3 +14,32 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// Get modal element
+var modal = document.getElementById("orderModal");
+var btn = document.querySelector(".cta");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// Handle form submission
+document.getElementById("orderForm").onsubmit = function (e) {
+  e.preventDefault();
+  alert("Pesanan Anda telah dikirim!");
+  modal.style.display = "none";
+};
